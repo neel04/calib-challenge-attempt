38650857fd3bc955810109765ed572ad4b837040
neel04 <neelgupta04@outlook.com>
2021-11-24 18:16:37 +0000

    updated network.py

--- main.py
+++ main.py
@@ -2,18 +2,15 @@
 # General
 from tqdm import tqdm
 import os
-import cv2
-import numpy as np
-import random
 
 # Torch Imports
-from torch.utils.data import DataLoader, BufferedShuffleDataset
-from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
 from torchvision.utils import save_image
+import pytorch_lightning as pl
 
 # File Imports
 from hvec import execute_shell, hevc_to_frames
-from network import Net
+from network import CalibNet
 from calib_dataset import CalibrationImageDataset
 
 if not os.path.isdir('/content/calib-challenge-attempt/calib-challenge'):
@@ -31,20 +28,18 @@ print(f'\nData Processing Complete! HVEC --> JPG\n')
 
 BATCH_SIZE = 2
 
-#PyTorch Dataset creation
 train_ds = CalibrationImageDataset('/content/calib-challenge-attempt/', files=[0,1,2,3])
-train_dataloader = DataLoader(train_ds, batch_size=BATCH_SIZE) #Making A dataloader from the fist 4 hvecs
-
-val_ds = CalibrationImageDataset('/content/calib-challenge-attempt/', files=[4])
-val_dataloader = DataLoader(val_ds, batch_size=BATCH_SIZE)              #Making A dataloader from the fist 4 hvecs
+train_dataloader = DataLoader(train_ds, batch_size=self.BATCH_SIZE) #Making A dataloader from the fist 4 hvecs
 
 #print(f'\nTrain Samples: {len(train_dataloader)}\nVal Samples: {len(val_dataloader)}\nBatch Size: {BATCH_SIZE}')
 
 img_1, tgt_1 = next(iter(train_dataloader))
 save_image(img_1[0], f'./train_sample.jpg')
 
-my_nn = Net()
-print('Model Architecture:\n',my_nn, end='\n')
+#Create the model and train it
+model = CalibNet(input_size=(376,1196), output_size=(2,), hidden_size=50, batch_size=BATCH_SIZE)
+trainer = pl.Trainer(max_epochs=10)
+trainer.fit(model)
 
 #======CLEANUP===========
 #Before Committing
--- network.py
+++ network.py
@@ -1,43 +1,80 @@
-# PyTorch libraries and modules
+# PyTorch modules and general stuff
+from calib_dataset import CalibrationImageDataset
+
 import torch
-from torch.autograd import Variable
+from torch.nn.modules.loss import MSELoss
+import pytorch_lightning as pl
 from torch import nn
-from torch.nn import ReLU, Softmax
-from torch.optim import Adam, SGD
 
-class Net(nn.Module):
-  def __init__(self):
-    super(Net, self).__init__()
-    self.conv1 = nn.Conv2d(1, 32, 3, 1)
-    self.conv2 = nn.Conv2d(32, 64, 3, 1)
-    self.dropout1 = nn.Dropout2d(0.25)
-    self.dropout2 = nn.Dropout2d(0.5)
-    self.fc1 = nn.Linear(9216, 128)
-    self.fc2 = nn.Linear(128, 10)
+class CalibNet(pl.LightningModule):
+    def __init__(self, input_size, output_size, hidden_size, batch_size):
+        super(CalibNet, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.BATCH_SIZE = batch_size
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(256*4*4, self.hidden_size)
+        self.fc2 = nn.Linear(self.hidden_size, self.output_size)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv4(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = x.view(-1, 256*4*4)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+    
+    def training_step(self, batch, batch_idx):
+        x, (y_1, y_2) = batch
+        y_hat_1, y_hat_2 = self.forward(x)
+        loss = MSELoss(y_hat_1, y_1) + MSELoss(y_hat_2, y_2)
+        tensorboard_logs = {'train_loss': loss}
+        self.log(f'train loss: {loss}')
+        return {'loss': loss, 'log': tensorboard_logs}
 
-  # x represents our data
-  def forward(self, x):
-    img, (tgt1, tgt2) = x
-    # Pass data through conv1
-    x = self.conv1(img)
-    # Use the rectified-linear activation function over x
-    x = F.relu(x)
+    def MAPELoss(self, output, target):
+      return torch.mean(torch.abs((target - output) / target))  
+    
+    def validation_step(self, batch, batch_idx):
+        x, (y_1, y_2) = batch
+        y_hat_1, y_hat_2 = self.forward(x)
+        mape_loss = self.MAPELoss(y_hat_1, y_1) + self.MAPELoss(y_hat_2, y_2)
 
-    x = self.conv2(x)
-    x = F.relu(x)
-
-    # Run max pooling over x
-    x = F.max_pool2d(x, 2)
-    # Pass data through dropout1
-    x = self.dropout1(x)
-    # Flatten x with start_dim=1
-    x = torch.flatten(x, 1)
-    # Pass data through fc1
-    x = self.fc1(x)
-    x = F.relu(x)
-    x = self.dropout2(x)
-    x = self.fc2(x)
-
-    # Apply softmax to x
-    output = F.log_softmax(x, dim=1)
-    return output_1, output_2
+        tensorboard_logs = {'MAPE:':mape_loss}
+        return {'val_loss': mape_loss, 'log':tensorboard_logs}
+    
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        tensorboard_logs = {'val_loss': avg_loss}
+        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
+    
+    def train_dataloader(self):
+        #Making A dataloader from the fist 4 hvecs
+        train_ds = CalibrationImageDataset('/content/calib-challenge-attempt/', files=[0,1,2,3])
+        train_dataloader = (train_ds, batch_size=self.BATCH_SIZE) #Making A dataloader from the fist 4 hvecs
+        return train_dataloader
+    
+    def val_dataloader(self):
+      #Making A dataloader from the last file
+      val_ds = CalibrationImageDataset('/content/calib-challenge-attempt/', files=[4])
+      val_dataloader = torch.utils.data.DataLoader(val_ds, batch_size=self.BATCH_SIZE)
