cfe55af035a19bfc6c2fba203beb0ca3f6afcb03
neel04 <neelgupta04@outlook.com>
2021-11-28 16:56:40 +0000

    Training achieved

--- network.py
+++ network.py
@@ -18,7 +18,7 @@ class CalibNet(pl.LightningModule):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(8192, self.hidden_size)
+        self.fc1 = nn.Linear(4096, self.hidden_size)
         self.fc2 = nn.Linear(self.hidden_size, self.output_size)
         self.relu = torch.nn.ReLU()
         self.maxpool1 = torch.nn.MaxPool2d(stride=4, kernel_size=3)
@@ -36,7 +36,7 @@ class CalibNet(pl.LightningModule):
         x = self.relu(x)
         x = self.maxpool3(x)
 
-        gate = x.view(-1)
+        gate = x.view(x.size(0), -1)
 
         z = self.fc1(gate)
         z = self.relu(z)
@@ -52,27 +52,23 @@ class CalibNet(pl.LightningModule):
         y_hat_1, y_hat_2 = self.forward(x)
 
         criterion = nn.MSELoss()
-        loss = criterion(y_hat_1, y_1) + criterion(y_hat_2, y_2)
+        loss = criterion(y_hat_1.view(-1).float(), y_1.float()) + criterion(y_hat_2.view(-1).float(), y_2.float())
         tensorboard_logs = {'train_loss': loss}
-        self.log(loss)
+        self.log('train loss:', loss)                #has to log a key-value pair ==> 'train loss:', loss
         return {'loss': loss, 'log': tensorboard_logs}
 
-    def MAPELoss(self, output, target):
-        print(f'\n\ntarget: {target}\toutput: {output.view(-1)}')
-        return torch.mean(torch.abs((target - output.view(-1)) / target))  
+    def MAPEMetric(self, output, target):
+        print(f'\noutput: {output.view(-1)}\ntarget: {target}')
+        return torch.mean(torch.abs((target.view(-1) - output.view(-1)) / target)) * 100
     
     def validation_step(self, batch, batch_idx):
         x, (y_1, y_2) = batch
         y_hat_1, y_hat_2 = self.forward(x)
-        criterion = self.MAPELoss
-        mape_loss = torch.tensor([])
-
-        for y1, y2, yhat1, yhat2 in zip(y_1, y_2, y_hat_1, y_hat_2):
-            print(f'\ny 1: {y1}\ty 2: {y2}')
-            mape_loss += (criterion(yhat1, y1) + criterion(yhat2, y2))
+        criterion = self.MAPEMetric
+        mape_loss = criterion(y_hat_1.view(-1).float(), y_1.float()) + criterion(y_hat_2.view(-1).float(), y_2.float())
 
         tensorboard_logs = {'MAPE:':mape_loss}
-        return {'val_loss': mape_loss, 'log':tensorboard_logs}
+        return {'val_loss': mape_loss.detach(), 'log':tensorboard_logs}
     
     def validation_epoch_end(self, outputs):
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
@@ -82,22 +78,15 @@ class CalibNet(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
-    
-    def collate_fn(self, batch):
-        for sample in batch:  #no empty data
-            assert sample[1][0] is not None
-        
-        out = [sample for sample in batch if sample[0] is not None and (sample[1][0] or sample[1][1])]
-        return torch.utils.data.dataloader.default_collate(out)
 
     def train_dataloader(self):
         #Making A dataloader from the fist 4 hvecs
         train_ds = nc.SafeDataset(CalibrationImageDataset('/content/calib-challenge-attempt/', files=[0,1,4,3]))
-        train_dataloader = torch.utils.data.DataLoader(train_ds, batch_size=self.batch_size, shuffle=True, num_workers=0) #Making A dataloader from the fist 4 hvecs
+        train_dataloader = torch.utils.data.DataLoader(train_ds, batch_size=self.batch_size, shuffle=True, num_workers=4) #Making A dataloader from the fist 4 hvecs
         return train_dataloader
     
     def val_dataloader(self):
       #Making A dataloader from the last file
       val_ds = nc.SafeDataset(CalibrationImageDataset('/content/calib-challenge-attempt/', files=[2]))  #2 is slightly different, hence good test for generalization
-      val_dataloader = torch.utils.data.DataLoader(val_ds, batch_size=self.batch_size,  num_workers=0)
+      val_dataloader = torch.utils.data.DataLoader(val_ds, batch_size=self.batch_size,  num_workers=4)
       return val_dataloader
