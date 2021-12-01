77f9a0a08f464f7807476f2b11c02c5b73c2c804
neel04 <neelgupta04@outlook.com>
2021-12-01 19:19:12 +0000

    removed artifacts + some docs

--- nas.py
+++ nas.py
@@ -23,26 +23,18 @@ if not os.path.isdir('/content/calib-challenge-attempt/data_3'):
 
 print(f'\nData Processing Complete! HVEC --> JPG\n')
 
-def convert_pytorch_dataloader_to_tf_dataset(dataloader, batch_size, shuffle=True):
-    dataset = tf.data.Dataset.from_generator(
-        lambda: dataloader,
-        output_types=(tf.float32, tf.float32),
-        output_shapes=(tf.TensorShape([256, 512]), tf.TensorShape([2,]))
-    )
-    if shuffle:
-        dataset = dataset.shuffle(buffer_size=len(dataloader.dataset))
-    dataset = dataset.batch(batch_size)
-    return dataset
-
 train_ds = nc.SafeDataset(CalibrationImageDataset('/content/calib-challenge-attempt/', files=[0,1,4,3])) #Making A dataset from the fist 4 hvecs
 
 val_ds = nc.SafeDataset(CalibrationImageDataset('/content/calib-challenge-attempt/', files=[2]))  #2 is slightly different, hence good test for generalization
 val_dataloader = torch.utils.data.DataLoader(val_ds, num_workers=4, batch_size=1)
 
-#train_dataloader = convert_pytorch_dataloader_to_tf_dataset(train_dataloader, batch_size=2)
-#val_dataloader = convert_pytorch_dataloader_to_tf_dataset(val_dataloader, batch_size=2)
-
 def data_generator(batch_size, dataset):
+    '''
+    Dataset Generator for easily constructing tf.Dataset;
+    adapter for a torch Dataset.
+    
+    returns (batch_sizes, 256, 512) + (batch_size, 2)
+    '''
     dloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
     for img, tgt in iter(dloader):
@@ -50,6 +42,7 @@ def data_generator(batch_size, dataset):
         img = img.numpy().reshape(batch_size, 256, 512)
         yield img, tgt
 
+# Creating the generator
 data_gen = data_generator(dataset=train_ds, batch_size=2)
     
 for image, tgt in data_gen:
