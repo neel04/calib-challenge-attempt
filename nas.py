3e891842c26cc4f241d2f5a3e3525a0d1da3873d
neel04 <neelgupta04@outlook.com>
2021-12-04 13:35:22 +0000

    Progress

--- nas.py
+++ nas.py
@@ -70,34 +70,47 @@ def data_generator(batch_size, dataset):
         yield img, tgt.squeeze(1)
 
 # Creating the generator
-BATCH_SIZE = 4
+BATCH_SIZE = 1
 
 train_data_gen = data_generator(dataset=train_ds, batch_size=BATCH_SIZE)
 val_data_gen = data_generator(dataset=val_ds, batch_size=BATCH_SIZE)
 
 def callable_iterator(generator, expected_batch_size):
     for img_batch, targets_batch in generator:
-        if img_batch.shape[0] == expected_batch_size and targets_batch.shape[1] == expected_batch_size:
+        if img_batch.shape[0] == expected_batch_size:
             yield img_batch, targets_batch
 
 train_dataset = tf.data.Dataset.from_generator(
     lambda: train_data_gen,
     output_types=(tf.float32, tf.float32), 
-    output_shapes=((256, 512), (2,))).batch(BATCH_SIZE, drop_remainder=True)
+    output_shapes=((256, 512), (2,))).batch(BATCH_SIZE, drop_remainder=True).unbatch()
 
 val_dataset = tf.data.Dataset.from_generator(
     lambda: val_data_gen,
     output_types=(tf.float32, tf.float32), 
-    output_shapes=((256, 512), (2,))).batch(BATCH_SIZE, drop_remainder=True)
+    output_shapes=((256, 512), (2,))).batch(BATCH_SIZE, drop_remainder=True).unbatch()
 
 for i,j in train_dataset.as_numpy_iterator():
-    print(f'\nTF dataset image shape: {i.shape}\nTF dataset target shape: {j.shape}\n\n')
+    print(f'\nTF dataset image shape: {i.shape}\nTF dataset target shape: {j.shape}\ntarget:{j}\n')
     break
 
 #=================================================================================================
 #Autokeras block
 
 # Initialize the multi with multiple inputs and outputs.
+def MAPEMetric(target, output):
+        return tf.math.reduce_mean(tf.math.abs((output - target) / output)) * 100
+
+model = ak.ImageRegressor(
+    output_dim=2,
+    loss="mean_squared_error",
+    metrics=[MAPEMetric],
+    project_name="image_regressor",
+    max_trials=100,
+    objective="val_loss",
+    overwrite=True,
+    seed=69420
+    )
 '''
 model = ak.AutoModel(
     inputs=[ak.ImageInput()],
@@ -109,28 +122,13 @@ model = ak.AutoModel(
     seed=69420
 )
 '''
-
-def MAPEMetric(target, output):
-        return np.mean(np.abs((output.view(-1) - target.view(-1)) / output)) * 100
-
-model = ak.ImageRegressor(
-    output_dim=2,
-    loss="mean_average_error",
-    metrics=[MAPEMetric],
-    project_name="image_regressor",
-    max_trials=100,
-    objective="val_loss",
-    overwrite=True,
-    seed=69420
-    )
-
 # Fit the model with prepared data.
 #Convert the TF Dataset to an np.array
 #Useless AutoKeras shenanigans
-#train_numpy = [list(mit.collapse(i)) for i in tfds.as_numpy(train_dataset)]
-#val_numpy = [list(mit.collapse(i)) for i in tfds.as_numpy(val_dataset)]
+#train_numpy = np.array([list(mit.collapse(i)) for i in tfds.as_numpy(train_dataset)])
+#val_numpy = np.array([list(mit.collapse(i)) for i in tfds.as_numpy(val_dataset)])
 
 #Setting up TRAINS logging
 #task = Task.init(project_name="CalibNet", task_name="Training CalibNet")
 
-model.fit(x=train_dataset, validation_dataset=val_dataset, epochs=3)
+model.fit(x=train_dataset, validation_data=val_dataset, epochs=3)
