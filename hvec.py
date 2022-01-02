9aaebef413d522c1a04c35efcac201613464d03c
neel04 <neelgupta04@outlook.com>
2022-01-02 15:59:11 +0000

    added `test_hevc_to_frames`

--- hvec.py
+++ hvec.py
@@ -38,7 +38,30 @@ def hevc_to_frames(sample_num, out_folder):
       outfile.write("".join(str(item) for item in cleaned))
   #print(f'{sample_num} has been completed')
 
+#extract hevc for testing dataset (unlabeled)
+def test_hevc_to_frames(sample_num, out_folder):
+  if not Path(f'./{out_folder}').exists():
+    execute_shell(f'mkdir {out_folder}')
+
+  vidcap = cv2.VideoCapture(f'/content/calib-challenge-attempt/calib_challenge/unlabeled/{sample_num}.hevc')   # sample_num --> int
+
+  success, image = vidcap.read()
+  
+  count = 0
+  
+  while success:
+    success, image = vidcap.read()
+
+    if not success:
+      break
+      
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(f"./{out_folder}/{count}.jpg", image)     # save frame as JPEG file
+    count += 1
+
+    
 def nan_i_nator(base_path, files):
+  #removes NANs in a dataset
   for file in files:
     lines = open(f'{base_path}/{file}.txt').readlines()
     idxes = []
